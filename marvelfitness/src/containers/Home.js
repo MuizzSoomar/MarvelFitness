880c735f2243c068ba3ccf1d0a9d1c26ca055804
Muizz Soomar <muizzsoomar@gmail.com>
2019-08-29 13:38:49 -0500

    Created Login/Homepage

---
+++ marvelfitness/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Marvel Fitness</h1>
+          <p>A new way of Fitness</p>
+        </div>
+      </div>
+    );
+  }
+}
