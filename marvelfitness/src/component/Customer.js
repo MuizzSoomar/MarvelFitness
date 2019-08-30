f62874a69cb0fe6bcdcef58fb41a5fb9cb131d67
Crystal <crystaljohnsonb@gmail.com>
2019-08-30 14:29:21 -0500

    Added rerouting to the Customer List
    The actual rerouting is commented out, but can be uncommented when the customer profile page is ready. (see "Customer.js" line 24

--- marvelfitness/src/component/Customer.js
+++ marvelfitness/src/component/Customer.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Redirect } from "react-router";
 
 class Customer extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            redirect: false
+        };
     }
 
+    handleCustomerClick = (e) => {
+        console.log(this.props.customer)
+        this.setState(() => {
+            return {
+                redirect: true
+            }
+        });
+    };
+
     render() {
+        if (this.state.redirect) {
+            // ********uncomment when the customer profile page is ready*******
+            // let link = "/customer/" + this.props.customer.customer_id;
+            // return <Redirect push to={link} />;
+        }
         return (
 
-                <tr>
+                <tr onClick={this.handleCustomerClick.bind(this)}>
 
                     <td>{this.props.customer.customer_id}</td>
                     <td>{this.props.customer.name}</td>
