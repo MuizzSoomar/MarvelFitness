56a5d90f093f07d084ae7b0fbfe3aef29639fe2a
Muizz Soomar <muizzsoomar@gmail.com>
2019-08-30 14:05:07 -0500

    Update Customer.js

--- marvelfitness/src/component/Customer.js
+++ marvelfitness/src/component/Customer.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Customer extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-
-                <tr>
-
-                    <td>{this.props.customer.customer_id}</td>
-                    <td>{this.props.customer.name}</td>
-
-                </tr>
-
-        )
-    }
+  render() {
+    return (
+      <tr>
+        <td>{this.props.customer.customer_id}</td>
+        <td>{this.props.customer.name}</td>
+      </tr>
+    );
+  }
 }
 
-export default Customer
+export default Customer;
